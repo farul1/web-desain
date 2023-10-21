a845cefd4f95138af3e5a5c7b6f5933530ce2e2c
Farul <72846589+farul1@users.noreply.github.com>
2023-10-22 02:39:04 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = function(){
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+}
+
+window.onscroll = function(){
+  menu.classList.remove('fa-times');
+  header.classList.remove('active');
+}
+
+
+const canvas = document.getElementById("star-canvas");
+const ctx = canvas.getContext("2d");
+
